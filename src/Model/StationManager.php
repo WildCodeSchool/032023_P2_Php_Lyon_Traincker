6c8630afde945f27b687d2a6ae3627bd7d38118e
Axx972 <123100991+Axx972@users.noreply.github.com>
2023-04-23 14:37:29 +0200

    not fixed

--- src/Model/StationManager.php
+++ src/Model/StationManager.php
@@ -5,14 +5,4 @@ namespace App\Model;
 class StationManager extends AbstractManager
 {
     public const TABLE = 'station';
-
-    public function selectAll(string $orderBy = 'name', string $direction = 'ASC'): array
-    {
-        $query = 'SELECT * FROM ' . static::TABLE;
-        if ($orderBy) {
-            $query .= ' ORDER BY ' . $orderBy . ' ' . $direction;
-        }
-
-        return $this->pdo->query($query)->fetchAll();
-    }
 }
