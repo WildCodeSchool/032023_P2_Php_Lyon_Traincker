b7dfa75ff5cc622a9d2d4aa2f955c7b1fc7271b7
val-fr69 <[valentin.inacio@live.fr](mailto:valentin.inacio@live.fr)>
2023-05-04 13:09:56 +0200

    add errors

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,20 +8,34 @@ class UserController extends AbstractController
 {
     public function login(): string
     {
+        $errors = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $credentials = array_map('trim', $_POST);
-            $userManager = new UserManager();
-            $user = $userManager->selectOneByEmail($credentials['login']);
-            if ($user && password_verify($credentials['password'], $user['password'])) {
-                $_SESSION['user_id'] = $user['id'];
-                header('Location: /');
-                exit();
-            } else {
-                die("C'est pas le bon mot de passe !");
+
+            if (!isset($credentials['login']) || empty($credentials['login'])) {
+                $errors[] = 'Remplissez le champ email !';
+            }
+            if (!isset($credentials['password']) || empty($credentials['password'])) {
+                $errors[] = 'Remplissez le mot de passe !';
+            }
+
+            if (empty($errors)) {
+
+                $userManager = new UserManager();
+                $user = $userManager->selectOneByEmail($credentials['login']);
+
+                if ($user && password_verify($credentials['password'], $user['password'])) {
+                    $_SESSION['user_id'] = $user['id'];
+                    header('Location: /');
+                    exit();
+                }
+                $errors[] = 'Email ou mot de passe invalide !';
             }
         }
 
-        return $this->twig->render('User/login.html.twig');
+        return $this->twig->render('User/login.html.twig', [
+            'errors' => $errors
+        ]);
     }
 
     public function logout()
